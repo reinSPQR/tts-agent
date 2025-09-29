61f7b9f44513272ad2175f4d6cfb18d8e4fda8b0
reinSPQR <rein1605@bvm.network>
2025-09-29 10:49:55 +0700

    Add model download scripts

---
+++ scripts/download_model.sh
@@ -0,0 +1,2 @@
+hf download --repo-id bosonai/higgs-audio-v2-generation-3B-base --local-dir ./checkpoints/bosonai/higgs-audio-v2-generation-3B-base
+hf download --repo-id bosonai/higgs-audio-v2-tokenizer --local-dir ./checkpoints/bosonai/higgs-audio-v2-tokenizer
